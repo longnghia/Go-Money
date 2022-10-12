90fc634298a16d1eb8dfcc0dd379279a16c74ae7
PaulNguyen <paul.nguyen.goldenowl@gmail.com>
2022-10-12 09:42:54 +0700

    init project

---
+++ GoMoney/Scences/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GoMoney
+//
+//  Created by Golden Owl on 12/10/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
