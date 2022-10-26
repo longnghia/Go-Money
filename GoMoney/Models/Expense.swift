e4cf6db7a3e0ba467213e9815c1ca423cd6d91e4
paul-nguyen-goldenowl <104960766+paul-nguyen-goldenowl@users.noreply.github.com>
2022-10-26 21:03:36 +0700

    fix: change expense structure (#14)

--- GoMoney/Models/Expense.swift
+++ GoMoney/Models/Expense.swift
@@ -4,18 +4,16 @@ import UIKit
 class Expense: Object {
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var type: String
-    @Persisted var title: String
     @Persisted var tag: String
     @Persisted var amount: Double
-    @Persisted var note: String?
+    @Persisted var note: String
     @Persisted var occuredOn: Date
     @Persisted var createdAt: Date?
     @Persisted var updatedAt: Date?
 
-    convenience init(type: ExpenseType = .expense, title: String = "Expense \(DateFormatter.today)", tag: TransactionTag = .food, amount: Double, note: String? = nil, occuredOn: Date, createdAt: Date? = Date(), updatedAt: Date? = nil) {
+    convenience init(type: ExpenseType = .expense, tag: TransactionTag = .food, amount: Double, note: String = "", occuredOn: Date, createdAt: Date? = Date(), updatedAt: Date? = nil) {
         self.init()
         self.type = type.rawValue
-        self.title = title
         self.tag = tag.getName()
         self.amount = amount
         self.note = note
