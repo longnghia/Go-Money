fc0837db5e9cffd968982de12d54a052bc5ba3af
paul-nguyen-goldenowl <104960766+paul-nguyen-goldenowl@users.noreply.github.com>
2022-11-01 17:45:32 +0700

    feat: share text & image (#24)
    * share text & image
    
    * remove metadata

--- GoMoney/Extensions/UIView+Extension.swift
+++ GoMoney/Extensions/UIView+Extension.swift
@@ -80,4 +80,11 @@ extension UIView {
             }
         }
     }
+
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
 }
